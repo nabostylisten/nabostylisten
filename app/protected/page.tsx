a86c884ac1b4bdef0176ea04387e35ced6af30b2
magnusrodseth <magnus.rodseth@gmail.com>
2025-08-01 15:31:15 +0200

    add zustand

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -4,6 +4,7 @@ import { createClient } from "@/lib/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { FetchDataSteps } from "@/components/tutorial/fetch-data-steps";
 import { CurrentUserAvatar } from "@/components/current-user-avatar";
+import StoreDemo from "@/components/store-demo";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -23,6 +24,8 @@ export default async function ProtectedPage() {
         </div>
 
         <CurrentUserAvatar />
+
+        <StoreDemo />
       </div>
       <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">Your user details</h2>
